14e107795e7db52179a97662db2e2bd9bbb622bc
José Joaquín Atria <jjatria@cpan.org>
2025-05-14 23:02:17 +0000

    Avoid setting undefined scheme attribute

--- lib/Mojolicious/Plugin/OpenTelemetry.pm
+++ lib/Mojolicious/Plugin/OpenTelemetry.pm
@@ -20,7 +20,7 @@ sub register ( $, $app, $config, @ ) {
 
         my $tx      = $c->tx;
         my $req     = $tx->req;
-        my $url     = $req->url;
+        my $url     = $req->url->to_abs;
         my $route   = $c->match->endpoint->to_string;
         my $query   = $url->query->to_string;
         my $method  = $req->method;
