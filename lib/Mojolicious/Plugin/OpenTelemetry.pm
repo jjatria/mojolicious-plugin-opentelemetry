34fc2812d4e5d0bf06053397dff0be2efd506249
José Joaquín Atria <jjatria@cpan.org>
2025-05-15 13:13:38 +0100

    Bump version

--- lib/Mojolicious/Plugin/OpenTelemetry.pm
+++ lib/Mojolicious/Plugin/OpenTelemetry.pm
@@ -1,7 +1,7 @@
 package Mojolicious::Plugin::OpenTelemetry;
 # ABSTRACT: An OpenTelemetry integration for Mojolicious
 
-our $VERSION = '0.005';
+our $VERSION = '0.006';
 
 use Mojo::Base 'Mojolicious::Plugin', -signatures;
 
